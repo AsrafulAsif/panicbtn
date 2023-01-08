ba9d7f8827b3178f011a7d028ffce86dc8e44556
AsrafulAsif <asifasrafulislam@gmail.com>
2023-01-08 18:07:45 +0600

    first commit

---
+++ lib/models/notification_model.dart
@@ -0,0 +1,13 @@
+class NotificationList {
+  final String panicType;
+  final String buttonpressedDate;
+  final String buttonpressedTime;
+
+
+  NotificationList({
+    required this.panicType,
+    required this.buttonpressedDate,
+    required this.buttonpressedTime,
+
+  });
+}
